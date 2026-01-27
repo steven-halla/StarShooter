db35d1e80b198a5db32f014570ae20a4311b8e2b
steven-halla <steven.r.halla@gmail.com>
2026-01-27 15:51:16 -0800

    level 9 base created

--- Levels/LevelNine.py
+++ Levels/LevelNine.py
@@ -4,6 +4,7 @@ from Constants.GlobalConstants import GlobalConstants
 from Entity.Bosses.BossLevelOne import BossLevelOne
 from Entity.Monsters.BileSpitter import BileSpitter
 from Entity.Monsters.BladeSpinners import BladeSpinner
+from Entity.Monsters.TimeBomb import TimeBomb
 from Entity.Monsters.TriSpitter import TriSpitter
 from SaveStates.SaveState import SaveState
 from ScreenClasses.MissionBriefingScreenLevelTwo import MissionBriefingScreenLevelTwo
@@ -12,7 +13,7 @@ from ScreenClasses.VerticalBattleScreen import VerticalBattleScreen
 class LevelNine(VerticalBattleScreen):
     def __init__(self,textbox):
         super().__init__(textbox)
-        self.tiled_map = pytmx.load_pygame("./Levels/MapAssets/leveltmxfiles/level8.tmx")
+        self.tiled_map = pytmx.load_pygame("./Levels/MapAssets/leveltmxfiles/level9.tmx")
         self.tile_size: int = self.tiled_map.tileheight
         self.map_width_tiles: int = self.tiled_map.width
         self.map_height_tiles: int = self.tiled_map.height
@@ -23,9 +24,12 @@ class LevelNine(VerticalBattleScreen):
         self.camera.world_height = self.WORLD_HEIGHT
         self.camera_y = self.WORLD_HEIGHT - (window_height / self.camera.zoom)
         self.camera.y = float(self.camera_y)
+        self.map_scroll_speed_per_frame: float = .4  # move speed of camera
+        self.total_enemies = 40
         self.prev_enemy_count: int = None
         self.enemies_killed: int = 0
         self.time_limit_ms = 2 * 60 * 1000  # 2 minutes
+        self.missed_enemies = []
         self.game_over: bool = False
         self.level_complete = False
         self.save_state = SaveState()
@@ -52,8 +56,7 @@ class LevelNine(VerticalBattleScreen):
         self.starship = state.starship
         self.starship.x = player_x
         self.starship.y = player_y
-        self.starship.update_hitbox()
-        self.starship.update_hitbox()
+
         self.load_enemy_into_list(state)
         self.starship.shipHealth = 144
         self.save_state.capture_player(self.starship, self.__class__.__name__)
@@ -61,31 +64,39 @@ class LevelNine(VerticalBattleScreen):
 
     def update(self, state) -> None:
         super().update(state)
-        if not hasattr(self, "last_enemy_count"):
-            self.last_enemy_count = len(state.enemies)
-
-        self.last_enemy_count = len(state.enemies)
+        # print(self.missed_enemies)
         self.update_game_over_condition()
-        self.update_add_enemy_to_missed_list(state)
         self.update_enemy_helper(state)
-        self.update_extract_object_names()
         self.update_handle_level_complete(state)
+
 
     def draw(self, state):
         super().draw(state)
         font = pygame.font.Font(None, 28)
         current_enemies = len(state.enemies)
-        self.draw_enemy_counter(current_enemies, font, state)
         self.draw_player_and_enemy(state)
         self.draw_ui_panel(state.DISPLAY)
 
         pygame.display.flip()
 
 
+
+
     def update_enemy_helper(self, state):
+        screen_bottom = self.camera.y + (self.camera.window_height / self.camera.zoom)
 
         for enemy in list(state.enemies):
             enemy.update(state)
+
+            if enemy.y > screen_bottom:
+                if enemy not in self.missed_enemies:
+                    self.missed_enemies.append(enemy)
+                    print("enemy missed")
+                continue
+
+            if isinstance(enemy, BileSpitter):
+                enemy.is_active = True
+
 
             if self.starship.hitbox.colliderect(enemy.hitbox):
                 enemy.color = (135, 206, 235)
@@ -102,22 +113,24 @@ class LevelNine(VerticalBattleScreen):
                 state.enemies.remove(enemy)
 
     def update_handle_level_complete(self, state):
-        pass
-        # if (
-        #         not self.level_complete
-        #         and any(
-        #     enemy.__class__.__name__ == "BossLevelEight" and enemy.enemyHealth <= 0
-        #     for enemy in state.enemies
-        # )
-        # ):
-        #     self.level_complete = True
-        #     # next_level = MissionBriefingScreenLevelEight()
-        #     state.currentScreen = next_level
-        #     next_level.start(state)
+        if (
+                not self.level_complete
+                and any(
+            enemy.__class__.__name__ == "BossLevelNine" and enemy.enemyHealth <= 0
+            for enemy in state.enemies
+        )
+        ):
+            self.level_complete = True
+            # next_level = MissionBriefingScreenLevelNine()
+            # state.currentScreen = next_level
+            # next_level.start(state)
+
 
 
     def update_game_over_condition(self):
-        pass
+        if len(self.missed_enemies) > 9:
+            print("GAME OVER!!!")
+            self.game_over = True
 
     def draw_player_and_enemy(self, state):
         if not self.playerDead:
@@ -126,13 +139,14 @@ class LevelNine(VerticalBattleScreen):
             enemy.draw(state.DISPLAY, self.camera)
             enemy.draw_damage_flash(state.DISPLAY, self.camera)
 
+
     def load_enemy_into_list(self, state):
         state.enemies.clear()
 
         for obj in self.tiled_map.objects:
-            if obj.name == "level_8_boss":
-                enemy = BossLevelOne()
-            elif obj.name == "bile_spitter":
+            # if obj.name == "level_8_boss":
+            #     enemy = BossLevelEight()
+            if obj.name == "bile_spitter":
                 enemy = BileSpitter()
             else:
                 continue
